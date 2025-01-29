b4f18a857e496ae0eae30ab69fa87e7804e17f2e
Divyanshu Tripathi <divyanshutripathi321@gmail.com>
2025-01-29 20:10:51 +0530

    fix

--- utils/crmd_system.py
+++ utils/crmd_system.py
@@ -6,8 +6,9 @@ from groq import Groq
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-key = os.getenv("GROQ_API_KEY")
+# load_dotenv()
+# key = os.getenv("GROQ_API_KEY")
+key = os.secrets["GROQ_API_KEY"]
 client = Groq(api_key=key)
 nlp = spacy.load("en_core_web_md")
 dataset = pd.read_excel("data/Conversation_data.xlsx")
